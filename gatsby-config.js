639632cf48e617fbc44ec4791ff44427f5c53e74
Misha Tiurin <misha.tiurin@skyscanner.net>
2019-05-02 21:45:41 +0200

    Change site tile and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ var proxy = require('http-proxy-middleware')
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Науково-практична конференція з міжнародною участю «МАЛОІНВАЗИВНА ХІРУРГІЯ ТА УРОЛОГІЯ ДИТЯЧОГО ВІКУ 2019',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Сторінка з корисною інформацією щодо науково-практичної конференції з міжнародною участю «МАЛОІНВАЗИВНА ХІРУРГІЯ ТА УРОЛОГІЯ ДИТЯЧОГО ВІКУ 2019',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
