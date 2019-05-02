dd2158f0932c1b764d0a3781286ee36233152123
Misha Tiurin <mtyurin@gmail.com>
2019-05-03 00:44:20 +0200

    Fix template names and sections

--- src/cms/preview-templates/IndexPagePreview.js
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -9,9 +9,7 @@ const IndexPagePreview = ({ entry, getAsset }) => {
     return (
       <IndexPageTemplate
         image={data.image}
-        title={data.title}
         heading={data.heading}
-        subheading={data.subheading}
         description={data.description}
         intro={data.intro || { blurbs: [] }}
         mainpitch={data.mainpitch || {}}
