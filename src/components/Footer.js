35ab113cc234e9dacd78f3b465b161b56edcdf60
Misha Tiurin <mtyurin@gmail.com>
2019-05-04 23:07:19 +0200

    Change twitter to Oberig in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,7 @@ import { Link } from 'gatsby'
 
 import facebook from '../img/social/facebook.svg'
 import instagram from '../img/social/instagram.svg'
-import twitter from '../img/social/twitter.svg'
-import vimeo from '../img/social/vimeo.svg'
+import oberig from '../img/oberig.png'
 
 const Footer = class extends React.Component {
   render() {
@@ -68,18 +67,18 @@ const Footer = class extends React.Component {
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
-                <a title="twitter" href="https://twitter.com">
-                  <img
-                    className="fas fa-lg"
-                    src={twitter}
-                    alt="Twitter"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
                 <a title="instagram" href="https://instagram.com">
                   <img
                     src={instagram}
                     alt="Instagram"
+                    style={{ width: '1em', height: '1em' }}
+                  />
+                </a>
+                <a title="Oberig" href="https://oberig.ua">
+                  <img
+                    className="fas fa-lg"
+                    src={oberig}
+                    alt="Oberig"
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
